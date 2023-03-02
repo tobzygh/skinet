6720c1c93487ed9432d52f58e735d682ce42337f
tobzygh <tobzyunlimited@gmail.com>
2023-03-02 22:12:44 +0100

    End of Section 10

--- client/src/app/shop/shop.service.ts
+++ client/src/app/shop/shop.service.ts
@@ -27,6 +27,10 @@ export class ShopService {
     return this.http.get<Pagination<Product[]>>(this.baseUrl + 'products', {params});
   }
 
+  getsProduct(id: number) {
+    return this.http.get<Product>(this.baseUrl + 'products/' + id);
+  }
+
   getBrands() {
     return this.http.get<Brands[]>(this.baseUrl + 'products/brands');
   }
